1a0ada3434378ac25a1aa5a1da48029630d00f5a
ivannikovmn <local_host@mail.ru>
2023-07-20 19:56:56 +0600

    add RestAPI l-10(start for manager - l1-l3 registration, l4 - authorization, l5 - fix img)

--- app/auth/middlewares.js
+++ app/auth/middlewares.js
@@ -1,5 +1,6 @@
 
 const Role = require('./Role')
+const User = require('./User')
 // <Лишние>
 // const isAuth = (req, res, next) => {
 //     if(req.user) next()
@@ -27,8 +28,47 @@ const isManager = async (req, res, next) => {
     else res.status(403).send ({message: "Unauthorized"})
 }
 
+const validateSignUp = async (req, res, next) => {
+    let errors = {}
+
+    if(!req.body.email || req.body.email.length === 0){
+        errors.email = "Поле Email обязательное"
+    }
+    if(!req.body.full_name || req.body.full_name.length === 0){
+        errors.full_name = "Поле Имя и Фамилия обязательное"
+    }
+    if(!req.body.company_name || req.body.company_name.length === 0){
+    // if(!req.body.company.name || req.body.company.name.length === 0){
+        errors.company_name = "Поле Имя компании обязательное"
+        // errors.company.name = "Поле Имя компании обязательное"
+    }
+    if(!req.body.password || req.body.password.length === 0){
+        errors.password = "Поле Пароль обязательное"
+    }
+    if(!req.body.password2 || req.body.password2.length === 0){
+        errors.password2 = "Поле Подтвердить пароль обязательное"
+    }    
+    if(req.body.password !== req.body.password2){
+        errors.password2 = "Пароли не совпадают"
+    }
+
+    const user = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+
+    if(user){
+        errors.email = "Пользователь с таким email уже зарегистрирован"
+    }
+
+    if(JSON.stringify(errors) !== JSON.stringify({})) res.status(400).send(errors)
+    else next()
+}
+
 module.exports = {
     // isAuth,
     isEmployee,
-    isManager
+    isManager,
+    validateSignUp
 }
