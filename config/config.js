9612d3be7ea5894b77b1d81b251fbe9a6af04d8e
ivannikovmn <local_host@mail.ru>
2023-08-11 19:22:00 +0600

    production mode configured

--- config/config.js
+++ config/config.js
@@ -8,6 +8,12 @@ module.exports = {
     },
     production: {
         // конфигурация для продакшн среды
+        username: 'doadmin',
+        password: 'AVNS_PjN5fJ8Q19zleegll5e',
+        database: 'defaultdb',
+        host: 'db-postgresql-sgp1-40737-do-user-14506858-0.b.db.ondigitalocean.com',
+        dialect: 'postgres',   
+        port: 25060     
     },
   };
   
--- config/db.js
+++ config/db.js
@@ -2,12 +2,21 @@ const { Sequelize } = require('sequelize');
 // const dbConfig = require('./config');
 // const dbConfig = require('./config')
 const dbConf = require('./config')
+let sequelize;
 
-const sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
+if(process.env === "production"){
+  sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
+    host: dbConf.production.host,
+    dialect: dbConf.production.dialect,
+    port: dbConf.production.port
+  });
+} else{
+  sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
     host: dbConf.development.host,
     dialect: dbConf.development.dialect,
-  }
-);
+  });
+}
+
 
 // Проверка соединения с базой данных
 sequelize
